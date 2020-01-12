4eaedb98fa05e52e78d81530f59a48d6ef4c4574
Adamski <Kamil.Adamski@unilever.com>
2020-01-12 15:02:58 +0100

    Commit with subscribe panell

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,49 +1,87 @@
 import React from 'react';
-import {Formik} from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
 
-const schema=Yup.object({
-    email:Yup.string().email("Please enter valid email.")
+const schema = Yup.object({
+    email: Yup.string().email("Please enter valid email.")
 })
 
-function Subscribe(){
-    return(
-        <Formik 
-        validationSchema={schema}
-        onSubmit={console.log}
-        initialValues={{
-            email:''
-        }}>
-        {
-            ({
-                handleSubmit,
-                handleChange,
-                values,
-                errors,
-            }) =>(
-                <Form noValidate onSubmit={handleSubmit}>
-                    <Form.Group controlId="validationFormikEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="Email"
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange}
-                            isInvalid={!!errors.email}
-                        />
-                        <Form.Control.Feedback>
-                            {errors.email}
-                        </Form.Control.Feedback>
-                    </Form.Group>
-                    <Button type="submit">Subscribe</Button>
-                </Form>
-            )
+class Subscribe extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            subscribed: false
         }
-        </Formik>
-    )
+    }
+
+    handleSubmit() {
+        this.setState({
+            subscribed: true
+        })
+    }
+
+
+    render() {
+        return (
+            <Jumbotron className="about-us d-flex align-items-center min-vh-100" style={{ fontFamily: "Lato" }}>
+                <Container className="text-center">
+                    {
+                        this.state.subscribed ?
+                            <Alert variant='success'>Thanks for subscribe</Alert> :
+                            <Container>
+                                <h1 style={{ fontSize: '4em' }}>Subscribe</h1>
+                                <Row>
+                                    <Col md={{ span: 4, offset: 4 }} lg={{ span: 4, offset: 4 }} xl={{ span: 4, offset: 4 }}>
+                                        <Formik
+                                            validationSchema={schema}
+                                            onSubmit={
+                                                this.handleSubmit.bind(this)
+                                            }
+                                            initialValues={{
+                                                email: ' '
+                                            }}>
+                                            {
+                                                ({
+                                                    handleSubmit,
+                                                    handleChange,
+                                                    values,
+                                                    errors,
+                                                }) => (
+                                                        <Form noValidate onSubmit={handleSubmit}>
+                                                            <Form.Group controlId="validationFormikEmail">
+                                                                <Form.Label>Email</Form.Label>
+                                                                <Form.Control
+                                                                    type="email"
+                                                                    placeholder="enter Email"
+                                                                    name="email"
+                                                                    value={values.email}
+                                                                    onChange={handleChange}
+                                                                    isInvalid={!!errors.email}
+                                                                />
+                                                                <Form.Control.Feedback type="invalid">
+                                                                    {errors.email}
+                                                                </Form.Control.Feedback>
+                                                            </Form.Group>
+                                                            <Button type="submit">Subscribe</Button>
+                                                        </Form>
+                                                    )
+                                            }
+                                        </Formik>
+                                    </Col>
+                                </Row>
+                            </Container>
+                    }
+                </Container>
+            </Jumbotron>
+        )
+    }
 }
 
 export default Subscribe;
